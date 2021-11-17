3816ca685ee3b4dc1392ceb63c4f09efc1abc484
Sugeng Priyanto <spriyanto360@gmail.com>
2021-11-17 15:13:36 +0700

    I don't know what the hell is wrong but now it's work properly

---
+++ client/src/components/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron bg-dark">
+                <h1>This is Jumbotron</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron
---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is a portfolio site by <a href="https://www.sugengpriyanto.com">Sugeng Priyanto</a>. Even though the features of this site are work properly, but <span>THIS IS NOT REAL ECOMMERCE SITE</span> and <span>DO NOT</span> make any purchase because the item listed on the page is not real. Thank you</p>
+        </div>
+    )
+}
